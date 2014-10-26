3ad5ab5fb2f301c49a0d24fabb16b023ecca0caa
JKnorre <jonas@knorre-erfurt.de>
2014-10-26 09:01:56 +0100

    Deutsch verändert

--- app/src/main/java/de/fivespecsdev/schoolapp/Deutsch.java
+++ app/src/main/java/de/fivespecsdev/schoolapp/Deutsch.java
@@ -1,18 +1,75 @@
 package de.fivespecsdev.schoolapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 
 public class Deutsch extends Activity {
+
+    private Spinner ChClassSpinner;
+    Button StartIPMSG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deutsch);
+
+        StartIPMSG = (Button) findViewById(R.id.BtnStartIPMSG);
+
+
+        String[] test = getResources().getStringArray(R.array.Faecher);
+
+        ChClassSpinner = (Spinner)findViewById(R.id.ChClassSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Deutsch.this,
+                android.R.layout.simple_spinner_item, test);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ChClassSpinner.setAdapter(adapter);
+        ChClassSpinner.setOnItemSelectedListener(this);
+
+        StartIPMSG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openIPMSG = getPackageManager().getLaunchIntentForPackage("com.coderplus.android.ipmsg");
+                startActivity(openIPMSG);
+            }
+        });
+
+
     }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+               Intent startStart = new Intent(Deutsch.this, start.class);
+               startActivity(startStart);
+                break;
+            case 1:
+                //Intent startDeutsch = new Intent(start.this, Deutsch.class);
+                //startActivity(startDeutsch);
+                break;
+            case 2:
+                // Whatever you want to happen when the third item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+}
 
 
     @Override
