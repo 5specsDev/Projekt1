a8a29d4c557891bc8e9af338605fe2dd318fd2d3
JKnorre <jonas@knorre-erfurt.de>
2014-10-26 11:04:08 +0100

    Physik Spinner fertig gestellt

--- app/src/main/java/de/fivespecsdev/schoolapp/Physik.java
+++ app/src/main/java/de/fivespecsdev/schoolapp/Physik.java
@@ -1,17 +1,107 @@
 package de.fivespecsdev.schoolapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 
-public class Physik extends Activity {
+public class Physik extends Activity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner ChClassSpinner;
+    Button StartIPMSG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physik);
+
+        StartIPMSG = (Button) findViewById(R.id.BtnStartIPMSG);
+
+        String[] faecher = getResources().getStringArray(R.array.FaecherPhysik);
+
+        ChClassSpinner = (Spinner)findViewById(R.id.ChClassSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Physik.this,
+                android.R.layout.simple_spinner_item, faecher);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ChClassSpinner.setAdapter(adapter);
+        ChClassSpinner.setOnItemSelectedListener(this);
+
+        StartIPMSG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openIPMSG = getPackageManager().getLaunchIntentForPackage("com.coderplus.android.ipmsg");
+                startActivity(openIPMSG);
+            }
+        });
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+
+                break;
+            case 1:
+                Intent startGeschichte = new Intent(Physik.this, Geschichte.class);
+                startActivity(startGeschichte);
+                break;
+            case 2:
+                Intent startLatein = new Intent(Physik.this, Latein.class);
+                startActivity(startLatein);
+                break;
+            case 3:
+                Intent startFranzoesisch = new Intent(Physik.this, Franzoesisch.class);
+                startActivity(startFranzoesisch);
+                break;
+            case 4:
+                Intent startReligion = new Intent(Physik.this, Religion.class);
+                startActivity(startReligion);
+                break;
+            case 5:
+                Intent startDeutsch = new Intent(Physik.this, Deutsch.class);
+                startActivity(startDeutsch);
+                break;
+            case 6:
+                Intent startMathe = new Intent(Physik.this, Mathe.class);
+                startActivity(startMathe);
+                break;
+            case 7:
+                Intent startEnglish = new Intent(Physik.this, English.class);
+                startActivity(startEnglish);
+                break;
+            case 8:
+                Intent startBiologie = new Intent(Physik.this, Biologie.class);
+                startActivity(startBiologie);
+                break;
+            case 9:
+                Intent startGeographie = new Intent(Physik.this, Geographie.class);
+                startActivity(startGeographie);
+                break;
+            case 10:
+                Intent startChemie = new Intent(Physik.this, Chemie.class);
+                startActivity(startChemie);
+                break;
+            case 11:
+                Intent startStart = new Intent(Physik.this, start.class);
+                startActivity(startStart);
+                break;
+
+
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
 
